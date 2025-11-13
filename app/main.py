e077a55a27b58a0de1aade21527c544ef3fa0b5d
goravjhabakh <goravjhabakh@gmail.com>
2025-11-13 12:25:24 +0530

    Backend v1.0

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import routes_target, routes_tracking, video_tracking
 
 app = FastAPI(title="Drone Object Tracking Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],     
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routes_target.router)
 app.include_router(routes_tracking.router)
